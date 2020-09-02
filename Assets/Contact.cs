c7e9639c230172792a875f5d7c8533e1b6261845
mchrpt <mchrpt@yahoo.com>
2020-09-01 23:14:53 -0400

    Initial commit

---
+++ Assets/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Contact : MonoBehaviour
+{
+    public static event Action<bool> teleport = delegate {};
+    public Boolean tp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+       tp = true;
+       teleport(tp);
+       Destroy(gameObject);
+    }
+}
---
+++ Assets/Teleport.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject newOb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Contact.teleport += triggerTeleport;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            newOb = Instantiate(prefab, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity) as GameObject;
+            newOb.GetComponent<Rigidbody2D>().AddRelativeForce(Vector3.left * 600);
+
+
+        }
+        else if(Input.GetKeyDown(KeyCode.E))
+        {
+            newOb = Instantiate(prefab, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity) as GameObject;
+            newOb.GetComponent<Rigidbody2D>().AddRelativeForce(Vector3.right * 600);
+        }
+    }
+    void triggerTeleport(bool isTrue)
+    {
+        transform.position = new Vector3(newOb.transform.position.x, newOb.transform.position.y + .5f, newOb.transform.position.z);
+    }
+}
